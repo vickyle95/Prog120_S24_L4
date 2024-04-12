3b850469e7c207b6dea7b9c02c7a29377ef6f22a
Vicky Le <vle04@student.rtc.edu>
2024-04-11 17:47:44 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Prog120_S24_L4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Todays Topics
+
+            // Quick Followup 
+            // Strings are weird
+            // Pedmas
+            int a = 1 - 2 * 5 / 7;
+            int b = 1 - 1 / 2 * 7;
+
+            // ---------------------------
+            // Floating Point Types ( float and double )
+            // Precision and Memory
+            // Decimal Type
+            // ---------------------------
+
+            // ---------------------------
+            // Revisit Parsing
+            // Casting
+            // Math with Doubles
+            // Rounding
+            // ---------------------------
+
+            // ---------------------------
+            // Question Examples
+            // Homework
+            // ----------------------------
+        }
+    }
+}
